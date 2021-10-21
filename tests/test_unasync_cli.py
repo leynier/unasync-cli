34937b1916c701d80af3cb046bac4d96d078e9e7
leynier <leynier41@gmail.com>
2021-10-20 20:37:03 -0400

    test: add mock test

--- tests/test_unasync_cli.py
+++ tests/test_unasync_cli.py
@@ -1,11 +1,8 @@
 from typer.testing import CliRunner
-
 from unasync_cli.main import app
 
 runner = CliRunner()
 
 
 def test_unasync_cli():
-    result = runner.invoke(app)
-    assert result.exit_code == 0
-    assert "Hello World!" in result.stdout
+    assert runner.invoke(app, ["--help"]).exit_code == 0
